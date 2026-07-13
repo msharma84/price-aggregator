92394c2ae90f61693f170271fc2abe9be3ebc06b
Mohit Sharma <mohitsharma603@gmail.com>
2026-07-13 14:04:29 +0530

    Initial Commit

---
+++ src/main/java/com/grisham/PriceAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.grisham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grisham/PriceAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grisham;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class PriceAggregatorApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
